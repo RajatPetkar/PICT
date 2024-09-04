807bb40167555caa9f7362b9cbbf08378d8fa7f9
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-09-04 22:51:14 +0530

    Update 21460_ASS4.py

--- DS LAB/21460_ASS4.py
+++ DS LAB/21460_ASS4.py
@@ -40,11 +40,54 @@ class Search:
                 right = mid - 1
         return "The value does not exist in the list"
         
-    def fibo(self,n):
-        if(n==1 or n == 0):
-            return n
+    def sentinelSearch(self, arr, item):
+        n = self.getLength(arr)
+        last = arr[-1]
+        arr[-1] = item
+        i=0
+        while arr[i] != item:
+            i+=1
+        arr[-1] = last
+        if i<n-1 or arr[-1]==item:
+            return i
         else:
-            return self.fibo(n-1) + self.fibo(n-2)
+            return -1
+
+    def fibonacciSearch(self, arr, item):
+        n = self.getLength(arr)
+
+        fib2 = 0
+        fib1 = 1
+        fib = fib2 + fib1
+
+        while fib < n:
+            fib2 = fib1
+            fib1 = fib
+            fib = fib2 + fib1
+
+        offset = -1
+
+        while fib > 1:
+            temp = offset + fib2
+            
+            i = temp if temp<n-1 else n-1
+
+            if arr[i] == item:
+                return i
+            elif arr[i]<item:
+                fib = fib1
+                fib1 = fib2
+                fib2 = fib - fib1
+                offset = i
+            else:
+                fib = fib2
+                fib1 = fib1 - fib2
+                fib2 = fib - fib1
+
+        if fib1 and offset + 1 < n and arr[offset + 1] == item:
+            return offset + 1
+        
+        return -1
         
             
 
@@ -55,5 +98,3 @@ value = int(input("The value to search for is:"))
 
 s.linear_search(list,value)
 print(s.binary_search(list,value))
-
-print(s.fibo(3))
