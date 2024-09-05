19b4206ffc2b73e5f4dc5e8d7bf12be171da00cf
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-09-05 11:41:29 +0530

    Update mainwindow.h

--- CGL/CG3/cg_assign_3/clipping/mainwindow.h
+++ CGL/CG3/cg_assign_3/clipping/mainwindow.h
@@ -3,43 +3,31 @@
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui
-{
-    class MainWindow;
+namespace Ui {
+class MainWindow;
 }
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    void dda(float x1, float y1, float x2, float y2);
-    void dda2(float x1, float y1, float x2, float y2);
-    int regioncode(double,double);
-    bool clipping(double &, double &, double &, double &);
-    void window();
-    void window1();
-    double x1,y1,x2,y2;
-
-    int code1,code2;
-    int xmin=100,xmax=200,ymin=100,ymax=200;
-    const int Top=8,Bot=4,Right=2,Left=1;
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+void mousePressEvent(QMouseEvent *ev);
+void dda(double x1,double y1,double x2,double y2,QRgb val);
+void ddaline(double x1,double y1,double x2,double y2,QRgb val);
+int sign(int x);
+void cohen(int x1,int y1,int x2,int y2);
+int code(int x1,int y1);
 private slots:
     void on_pushButton_clicked();
 
-
-    void on_pushButton_2_clicked();
-
-    void on_pushButton_3_clicked();
-
-    void on_pushButton_4_clicked();
-
 private:
     Ui::MainWindow *ui;
+    bool start;
+    int ver,a[100],b[100];
 };
+
 #endif // MAINWINDOW_H
+
