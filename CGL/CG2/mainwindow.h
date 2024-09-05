f290264091510ff2195105d5621a6d8714867326
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-09-05 11:39:31 +0530

    Update mainwindow.h

--- CGL/CG2/mainwindow.h
+++ CGL/CG2/mainwindow.h
@@ -16,20 +16,17 @@ public:
     ~MainWindow();
 
 private slots:
-    void line(int ,int ,int ,int);
-
-    void on_pushButton_2_clicked();
-
     void on_pushButton_clicked();
-
-
+    void mousePressEvent(QMouseEvent *event);
+    void line(int,int,int,int);
+    void on_pushButton_2_clicked();
 
 
 private:
     Ui::MainWindow *ui;
     int count;
     bool start;
-    int a[100],b[100],ver,i,xi[20],j,temp,k,m,n;
-    float slope[20];
+    int a[100],b[100],ver,i,xi[20],j,temp;
+    float slope[20],dx,dy;
 };
 #endif // MAINWINDOW_H
