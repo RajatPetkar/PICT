eb8fd0fcb288f646315e3956cb98b202752b8024
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-09-09 05:48:17 +0530

    Update 21460_ASS6.py

--- DS LAB/21460_ASS6.py
+++ DS LAB/21460_ASS6.py
@@ -8,32 +8,25 @@ class Sorter:
     def swap(self, a, b, arr):
         arr[a], arr[b] = arr[b], arr[a]
 
-    def partition(self, elements, start, end):
-        pivot_index = start
-        pivot = elements[pivot_index][0]
+    def partition(arr, low, high):
+    pivot = arr[high]
+    
+    i = low - 1
 
-        while start < end:
-            while start < self.length(elements) and elements[start][0] <= pivot:
-                start += 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
 
-            while elements[end][0] > pivot:
-                end -= 1
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
-            if start < end:
-                self.swap(start, end, elements)
-
-        self.swap(pivot_index, end, elements)
-
-        return end
-
-    def quickSort(self, elements, start, end, pass_num=1):
-        if start < end:
-            pi = self.partition(elements, start, end)
-            print(f"Pass {pass_num}: {elements}")  # Print each pass
-            pass_num += 1  # Increment pass number
-            self.quickSort(elements, start, pi-1, pass_num)
-            self.quickSort(elements, pi+1, end, pass_num)
-        return elements
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        print(f"{} Pass :- ")
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
 
 
 n = int(input("Enter no. of students: "))
