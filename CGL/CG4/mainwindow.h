7107553758dca65034b9283638f84bbfb655ac7e
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2024-10-17 07:51:58 +0530

    Add files via upload

---
+++ CGL/CG4/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void ddaline(float, float, float, float);
+    void drawTriangle(float, float, float, float, float, float);
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    float x1, y1, x2, y2, x3, y3;
+};
+#endif // MAINWINDOW_H
