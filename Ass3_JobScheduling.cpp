06fb40c37e5699d2a5a7a8667e1181913d2b80cb
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2026-05-14 10:10:11 +0530

    Update Ass3_JobScheduling.cpp

--- Ass3_JobScheduling.cpp
+++ Ass3_JobScheduling.cpp
@@ -1,32 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 struct Job {
     char id;
-    int deadline, profit;
+    int deadline;
+    int profit;
 };
-
+// Comparator for sorting by profit
 bool cmp(Job a, Job b) {
     return a.profit > b.profit;
 }
 
 int main() {
-    Job arr[] = {{'A',4,100}, {'B',1,190}, {'C',3,27}, {'D',2,25}};
-    int n = 4;
+    int n;
+    cout << "Enter number of jobs: ";
+    cin >> n;
+    Job arr[n];
 
-    sort(arr, arr+n, cmp);
-
-    int slot[3] = {0};
-
+    cout << "\nEnter Job ID Deadline Profit\n";
     for(int i = 0; i < n; i++) {
-        for(int j = arr[i].deadline-1; j >= 0; j--) {
+        cin >> arr[i].id
+            >> arr[i].deadline
+            >> arr[i].profit;
+    }
+    // Sort jobs by maximum profit
+    sort(arr, arr + n, cmp);
+
+    // Find maximum deadline
+    int maxDeadline = 0;
+    for(int i = 0; i < n; i++) {
+        maxDeadline = max(maxDeadline,arr[i].deadline);
+    }
+    // Slot array
+    vector<char> slot(maxDeadline, 0);
+    int totalProfit = 0;
+    // Job Scheduling
+    for(int i = 0; i < n; i++) {
+        for(int j = arr[i].deadline - 1;j >= 0;j--) {
             if(slot[j] == 0) {
                 slot[j] = arr[i].id;
+                totalProfit += arr[i].profit;
                 break;
             }
         }
     }
+    // Output scheduled jobs
+    cout << "\nScheduled Jobs:\n";
 
-    for(int i = 0; i < 3; i++)
-        if(slot[i]) cout << (char)slot[i] << " ";
+    for(int i = 0; i < maxDeadline; i++) {
+        if(slot[i] != 0)
+            cout << slot[i] << " ";
+    }
+    cout << "\n\nTotal Profit = "
+         << totalProfit;
+
+    return 0;
 }
