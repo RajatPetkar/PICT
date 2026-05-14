525928c9bc5fb3860714084f886867528cb4b995
Rajat Petkar <110027873+RajatPetkar@users.noreply.github.com>
2026-05-14 10:11:33 +0530

    Update Ass3_Kruskals.cpp

--- Ass3_Kruskals.cpp
+++ Ass3_Kruskals.cpp
@@ -1,37 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 struct Edge {
-    int u, v, w;
+    int u;
+    int v;
+    int w;
 };
-
+// Comparator for sorting edges
 bool cmp(Edge a, Edge b) {
     return a.w < b.w;
 }
 
 int parent[100];
-
 int find(int x) {
-    if(parent[x] == x) return x;
+    if(parent[x] == x)
+        return x;
     return parent[x] = find(parent[x]);
 }
-
 void unite(int a, int b) {
     parent[find(a)] = find(b);
 }
-
 int main() {
-    Edge e[] = {{0,1,2}, {0,3,6}, {1,2,3}, {1,3,8}, {1,4,5}, {2,4,7}};
-    int n = 6;
+    int vertices, edges;
+    cout << "Enter number of vertices: ";
+    cin >> vertices;
 
-    for(int i = 0; i < 100; i++) parent[i] = i;
+    cout << "Enter number of edges: ";
+    cin >> edges;
+    Edge e[edges];
 
-    sort(e, e+n, cmp);
+    cout << "\nEnter source destination weight:\n";
+    for(int i = 0; i < edges; i++) {
+        cin >> e[i].u
+            >> e[i].v
+            >> e[i].w;
+    }
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < 100; i++) {
+        parent[i] = i;
+    }
+
+    sort(e, e + edges, cmp);
+
+    int totalCost = 0;
+    cout << "\nMinimum Spanning Tree:\n";
+    // Kruskal Algorithm
+    for(int i = 0; i < edges; i++) {
+
         if(find(e[i].u) != find(e[i].v)) {
-            cout << e[i].u << "-" << e[i].v << " ";
+
+            cout << e[i].u
+                 << " - "
+                 << e[i].v
+                 << " : "
+                 << e[i].w
+                 << endl;
+
+            totalCost += e[i].w;
+
             unite(e[i].u, e[i].v);
         }
     }
+
+    cout << "\nTotal Minimum Cost = "
+         << totalCost;
+
+    return 0;
 }
